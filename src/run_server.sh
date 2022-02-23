cf4fb007b0c4beb11bfc555abca20696a16d6201
Renegade-Master <renegade.master.dev@protonmail.com>
2022-02-23 18:41:44 +0000

    Corrected License; Added Env Configs;
    - .github/workflows/docker-build.yml
    - LICENSE
    - docker/zomboid-dedicated-server.Dockerfile
    - src/install_server.scmd
    - src/run_server.sh
      - Clarified source License. License unchanged from GPL v3.
    
    - README.md
    - docker-compose.yaml
      - Clarified source License. License unchanged from GPL v3.
      - Added info for new `chown` related config options.

--- src/run_server.sh
+++ src/run_server.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+
+#
+#  Project Zomboid Dedicated Server using SteamCMD Docker Image.
+#  Copyright (C) 2021-2022 Renegade-Master [renegade.master.dev@protonmail.com]
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 #######################################################################
 #   Author: Renegade-Master
 #   Contributors: JohnEarle
@@ -7,7 +26,7 @@
 #######################################################################
 
 # Set to `-x` for Debug logging
-set +x
+set +x -u -o pipefail
 
 # Start the Server
 function start_server() {
